2b7c8ce6eec616c3cba10b158018afa330ad5e56
Kane <kaneboswell@proton.me>
2026-04-13 14:16:06 +0000

    feat(src): npm run init

--- src/01-core.js
+++ src/01-core.js
@@ -1,80 +1,22 @@
-import { colorBlock, sayHelloImpl } from './02-example-module.js';
+// CobaltVDisk
+// Author: Triflare
+// Initialized by npm run init
 
+/**
+ * Blank starter template.
+ *
+ * Usage example:
+ * - Add a block object in getInfo().blocks.
+ * - Add the matching method on the class.
+ */
+/* global Scratch */
 class TurboWarpExtension {
-  constructor() {
-    this.runtime = null;
-  }
-
   getInfo() {
     return {
-      id: 'myTurboWarpExtension',
-      name: Scratch.translate('My Extension'),
-      color1: '#4CAF50',
-      color2: '#45a049',
-      color3: '#3d8b40',
-      menuIconURI: mint.assets.get('icons/menu.png') ?? '',
-      blockIconURI: mint.assets.get('icons/block.png') ?? '',
-      blocks: [
-        {
-          opcode: 'helloWorld',
-          blockType: 'reporter',
-          text: Scratch.translate('hello world'),
-        },
-        {
-          opcode: 'add',
-          blockType: 'reporter',
-          text: Scratch.translate('[A] + [B]'),
-          arguments: {
-            A: {
-              type: 'number',
-              defaultValue: 0,
-            },
-            B: {
-              type: 'number',
-              defaultValue: 1,
-            },
-          },
-        },
-        {
-          opcode: 'colorBlock',
-          blockType: 'reporter',
-          text: Scratch.translate('selected color [COLOR]'),
-          arguments: {
-            COLOR: {
-              type: 'color',
-              defaultValue: '#FF0000',
-            },
-          },
-        },
-        {
-          opcode: 'sayHello',
-          blockType: 'reporter',
-          text: Scratch.translate('say hello to [NAME]'),
-          arguments: {
-            NAME: {
-              type: 'string',
-              defaultValue: 'world',
-            },
-          },
-        },
-      ],
+      id: "tfCobaltvdisk",
+      name: Scratch.translate("CobaltVDisk"),
+      blocks: [],
     };
-  }
-
-  sayHello(args) {
-    return sayHelloImpl(args);
-  }
-
-  helloWorld() {
-    return 'hello world!';
-  }
-
-  add(args) {
-    return Number(args.A) + Number(args.B);
-  }
-
-  colorBlock(args) {
-    return colorBlock(args);
   }
 }
 
